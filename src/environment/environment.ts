01837c5d4648c3f4ad927fc18862af4cc1573bfc
Matteo Di Blasio <matteodibs@gmail.com>
2023-07-25 16:54:33 +0200

    production env

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_URL: 'http://localhost:8080/',
-    SITE_URL: "http://localhost:5173"
+    API_URL: 'http://172.16.10.250:8081/',
+    SITE_URL: "http://172.16.10.250:3000"
 }
