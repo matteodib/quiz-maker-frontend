2c0bf74b129f5af387a0744a91af6259b69346bb
Matteo <matteodibs@gmail.com>
2025-02-11 17:26:34 +0100

    [fix] public env production

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,8 +1,8 @@
-import { VITE_PUBLIC_API_URL, VITE_PUBLIC_SITE_URL } from "$env/static/private";
+import { PUBLIC_API_URL, PUBLIC_SITE_URL } from "$env/static/public";
 
 
 export const environment = {
     //API_URL: 'http://r2u.online:8081/',
-    API_URL: VITE_PUBLIC_API_URL,
-    SITE_URL: VITE_PUBLIC_SITE_URL
+    API_URL: PUBLIC_API_URL,
+    SITE_URL: PUBLIC_SITE_URL
 }
